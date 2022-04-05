a1303be0d72a641eaf76730837da13f6da1060dd
Amy Gori <amydgori@gmail.com>
2022-04-05 13:33:37 -0400

    Add crud views and serializers for BookRecord and BookReview

--- api/views.py
+++ api/views.py
@@ -1,14 +1,31 @@
-from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.decorators import action, permission_classes
+from rest_framework.exceptions import PermissionDenied, ParseError
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-
-from .models import Book
-from .serializers import BookSerializer, BookRecordSerializer
+from .models import Book, BookRecord, BookReview
+from .serializers import (
+    BookSerializer,
+    BookDetailSerializer,
+    BookRecordSerializer,
+    BookReviewSerializer,
+)
+from .custom_permissions import (
+    IsAdminOrReadOnly,
+    IsReaderOrReadOnly,
+)
 
 
 class BookViewSet(ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    queryset = Book.objects.all().order_by("title")
+    serializer_class = BookDetailSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.action in ["list"]:
+            return BookSerializer
+        return super().get_serializer_class()
 
     @action(detail=False)
     def featured(self, request):
@@ -18,9 +35,25 @@ class BookViewSet(ModelViewSet):
 
 
 class BookRecordViewSet(ModelViewSet):
-    queryset = Book.objects.all()
+    queryset = BookRecord.objects.all()
     serializer_class = BookRecordSerializer
+    permission_classes = [IsAuthenticated, IsReaderOrReadOnly]
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        return queryset.filter(reader=self.request.user)
+        return queryset.filter(reader=self.request.user, book=self.kwargs["book_pk"])
+
+    def perform_create(self, serializer):
+        serializer.save(reader=self.request.user)
+
+
+class BookReviewListCreateView(ListCreateAPIView):
+    serializer_class = BookReviewSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return BookReview.objects.filter(book_id=self.kwargs["book_pk"])
+
+    def perform_create(self, serializer, **kwargs):
+        book = get_object_or_404(Book, pk=self.kwargs["book_pk"])
+        serializer.save(reviewed_by=self.request.user, book=book)
--- library/urls.py
+++ library/urls.py
@@ -14,19 +14,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
 from api import views as api_views
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedSimpleRouter
 
-router = DefaultRouter()
+
+router = DefaultRouter(trailing_slash=False)
 router.register("books", api_views.BookViewSet, basename="books")
-router.register(
+books_router = NestedSimpleRouter(router, "books", lookup="book")
+books_router.register(
     "book_records",
     api_views.BookRecordViewSet,
     basename="book_records",
 )
-urlpatterns = router.urls
 
-urlpatterns += [
+urlpatterns = [
+    path("api/", include(router.urls)),
+    path("api/", include(books_router.urls)),
+    path(
+        "api/books/<int:book_pk>/reviews",
+        api_views.BookReviewListCreateView.as_view(),
+        name="book_reviews",
+    ),
     path("admin/", admin.site.urls),
 ]
